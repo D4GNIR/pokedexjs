dbf6b3c86694067a0a14d5b2865ff2ea4a698abc
Jérémie CETTE <jc@isitecc.com>
2024-11-22 16:57:00 +0100

    ajout commentaire

--- details.js
+++ details.js
@@ -1,30 +1,43 @@
+// Récupere l'id du pokemon dans l'url
 const urlParams = new URLSearchParams(window.location.search);
+// Récupere l'id du pokemon
 const pokemonId = urlParams.get('id');
 
 async function loadPokemonDetails() {
+    // On récupere l'url du pokemon
     let url = "https://pokeapi.co/api/v2/pokemon/" + pokemonId;
+    // On récupere les données du pokemon
     let res = await fetch(url);
     let pokemon = await res.json();
-
+    // On récupere les élément grace à leurs ID
     document.getElementById("pokemon-name").innerText = pokemon.name.toUpperCase();
     document.getElementById("pokemon-img").src = pokemon.sprites.front_default;
     
-    // Types
+    // Ajout des types
     const typesDiv = document.getElementById("pokemon-types");
+    // Il peut y avoir plusieus type donc boucle foreach
     pokemon.types.forEach(type => {
+        // Création du span
         const typeSpan = document.createElement("span");
+        // On passe les types en majuscule
         typeSpan.innerText = type.type.name.toUpperCase();
+        // On ajoute la class type-box et le type
         typeSpan.classList.add("type-box", type.type.name);
+        // On ajoute le span au div
         typesDiv.appendChild(typeSpan);
     });
 
-    // Description
+    // On récupere la description
     const speciesRes = await fetch(pokemon.species.url);
     const speciesData = await speciesRes.json();
+    // On récupere les élément grace à leurs ID
     document.getElementById("pokemon-description").innerText = 
+        // On récupere la description   
         speciesData.flavor_text_entries[9].flavor_text;
 }
 
+// Si l'id du pokemon est présent
 if (pokemonId) {
+    // On charge les détails du pokemon
     loadPokemonDetails();
 }
--- home.js
+++ home.js
@@ -1,35 +1,43 @@
+// Nombre de pokemon
 const pokemonCount = 300;
+// Pokedex
 var pokedex = {};
 
 window.onload = async function() {
+    // Boucle pour récuperer les pokemon
     for (let i = 1; i <= pokemonCount; i++) {
+        // Récupere les données du pokemon
         await getPokemon(i);
+        // Création de la carte pokemon
         let pokemon = document.createElement("div");
         pokemon.id = i;
         pokemon.classList.add("pokemon-card");
-        
+        // Récupere l'image du pokemon
         let img = document.createElement("img");
         img.src = pokedex[i]["img"];
-        
+        // Récupere le nom du pokemon
         let name = document.createElement("div");
         name.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
-        
+        // Ajoute l'image et le nom au pokemon
         pokemon.appendChild(img);
         pokemon.appendChild(name);
-        
+        // Ajoute un écouteur d'événement pour le click
         pokemon.addEventListener("click", () => {
             window.location.href = `details.html?id=${i}`;
         });
-        
+        // Ajoute la carte au pokedex
         document.getElementById("pokemon-grid").appendChild(pokemon);
     }
 }
 
+// Récupere les données du pokemon
 async function getPokemon(num) {
+    // Récupere l'url du pokemon
     let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+    // Récupere les données du pokemon
     let res = await fetch(url);
     let pokemon = await res.json();
-
+    // Ajoute les données du pokemon dans la pokedex
     pokedex[num] = {
         "name": pokemon["name"],
         "img": pokemon["sprites"]["front_default"],
