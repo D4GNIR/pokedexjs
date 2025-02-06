75716524db12d9bad3d6e2c2089ac394a37106db
Matys Marous <MR@isitecc.com>
2025-02-06 11:52:11 +0100

    j' ai fini de fusinner les css et il y a toute les icon dans le home

--- home.js
+++ home.js
@@ -1,10 +1,7 @@
-// Nombre de pokemon à afficher
 const pokemonCount = 151;
-// Pokedex
 var pokedex = {};
 
 window.onload = async function () {
-  // Dictionnaire des types avec leurs icônes
   const typeIcons = {
     grass: '<i class="fa-brands fa-pagelines"></i>',
     poison: '<i class="fa-solid fa-skull-crossbones"></i>',
@@ -17,43 +14,44 @@ window.onload = async function () {
     ground: '<i class="fa-solid fa-archway"></i>',
     fairy: '<i class="fa-solid fa-feather"></i>',
     psychic: '<i class="fa-solid fa-eye"></i>',
-    rock: '<i class="fa-solid fa-hill-rockslide"></i>',
+    rock: '<i class="fa-solid fa-gem"></i>',
     dragon: '<i class="fa-solid fa-dragon"></i>',
     fighting: '<i class="fa-solid fa-user-ninja"></i>',
+    steel: '<i class="fa-solid fa-shield-halved"></i>',
+    dark: '<i class="fa-solid fa-moon"></i>',
+    ghost:'<i class="fa-solid fa-ghost"></i>',
+    ice: '<i class="fa-solid fa-icicles"></i>'
   };
 
-  // Fonction pour récupérer les données des Pokémon
   async function getPokemon(num) {
-    let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+    let url = `https://pokeapi.co/api/v2/pokemon/${num}`;
     let res = await fetch(url);
     let pokemon = await res.json();
     pokedex[num] = {
-      name: pokemon["name"],
-      img: pokemon["sprites"]["front_default"],
-      type: pokemon["types"][0]["type"]["name"], // Récupère le type principal
+      name: pokemon.name,
+      img: pokemon.sprites.front_default,
+      type: pokemon.types[0].type.name,
     };
   }
 
-
-  // Boucle pour créer les cartes Pokémon
   for (let i = 1; i <= pokemonCount; i++) {
     await getPokemon(i);
 
     let pokemon = document.createElement("div");
     pokemon.id = i;
     pokemon.classList.add("pokemon-card");
+    pokemon.classList.add(pokedex[i].type); // Classe du type
 
     let img = document.createElement("img");
-    img.src = pokedex[i]["img"];
-    img.alt = pokedex[i]["name"];
+    img.src = pokedex[i].img;
+    img.alt = pokedex[i].name;
 
     let name = document.createElement("div");
-    name.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+    name.innerText = `${i}. ${pokedex[i].name.toUpperCase()}`;
 
     let icon = document.createElement("div");
-    icon.innerText = pokedex[i]["type"]
+    icon.innerHTML = typeIcons[pokedex[i].type] || "❓";
     icon.classList.add("types");
-    
 
     pokemon.appendChild(img);
     pokemon.appendChild(name);
